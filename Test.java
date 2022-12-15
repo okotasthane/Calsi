69385f01744702bfa637755bce18ce99c1066eaf
adikote <118277398+adikote@users.noreply.github.com>
2022-12-15 23:45:33 +0530

    is working method added
    Added method with true as default value.

--- Test.java
+++ Test.java
@@ -3,4 +3,9 @@ public class Test {
         System.out.println("hello!");
         System.out.println("manual sysout");
     }
+
+    private boolean isWorking() {
+       return true; // default
+    }
 }
+
