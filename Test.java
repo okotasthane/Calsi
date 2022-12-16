d68e93a7fd58e366bc8f7c5fec7cd2a90338a7c8
adikote <118277398+adikote@users.noreply.github.com>
2022-12-16 12:46:37 +0530

    Added Calculate method
    Added calculate method to add 2 numbers

--- Test.java
+++ Test.java
@@ -3,10 +3,16 @@ public class Test {
         System.out.println("hello!");
         System.out.println("second sysout");
         System.out.println("manual sysout");
+        int sum = new Test().calculate(10,20);
+        System.out.println("calculate " + sum);
     }
 
     private boolean isWorking() {
        return true; // default
+    }
+    
+    private int calculate(int num1, int num2) {
+       return (num1 != null && num2 != null) ? num1 + num2 : 0; // 0  as default
     }
 }
 
