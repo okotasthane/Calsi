449a283b043905420b6261824578332b225b1d9e
adikote <118277398+adikote@users.noreply.github.com>
2022-12-16 16:12:11 +0530

    Test variable
    Test variable

--- Test.java
+++ Test.java
@@ -1,4 +1,7 @@
 public class Test {
+    
+    int test_variable = 10;
+    
     public static void main(String[] args) {
         System.out.println("hello!");
         System.out.println("second sysout");
