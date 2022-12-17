d961af061dca6ae32ad612d76d32c60c032737a2
Onkar <onkar.kotasthane@gmail.com>
2022-12-17 12:52:07 +0530

    added show msg method

--- src/com/TestSecond.java
+++ src/com/TestSecond.java
@@ -2,6 +2,11 @@ package src.com;
 
 public class TestSecond {
     public int multiple2(Integer num1, Integer num2) {
+        this.showMsg("great!");
         return num1 * num2;
+    }
+
+    private void showMsg(String msg) {
+        System.out.println(msg);
     }
 }
